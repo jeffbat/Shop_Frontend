68d9a5d1f54dac14157a2acfd372d52871298af4
jeffbat <75343055+jeffbat@users.noreply.github.com>
2021-09-07 14:30:03 -0400

    final push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 
 
@@ -19,6 +21,8 @@ function App() {
         <main>
             <Container>
                 <Route path='/' component={HomeScreen} exact/>
+                <Route path='/login' component={LoginScreen} exact/>
+                <Route path='/register' component={RegisterScreen} exact/>
                 <Route path="/product/:id" component={ProductScreen} />
                 <Route path="/cart/:id?" component={CartScreen} />
             </Container>
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
-
-
-
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 
 
@@ -16,7 +13,7 @@ export const cartReducer = (state={cartItems:[]}, action) => {
                 return{
                     ...state,
                     cartItems: state.cartItems.map(x =>
-                        x.product ===existItem.product ? item : x
+                        x.product === existItem.product ? item : x
                     )
                 }
 
@@ -25,6 +22,12 @@ export const cartReducer = (state={cartItems:[]}, action) => {
                     ...state,
                     cartItems:[...state.cartItems, item]
                 }
+            }
+
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems:state.cartItems.filter(x => x.product !== action.payload)
             }
 
         default:
